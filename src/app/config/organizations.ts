f393281d1a4226fa11b2aabe050f7e0602bff251
wonsunahn <54322243+wonsunahn@users.noreply.github.com>
2025-09-04 18:00:30 -0400

    Configured my organizations.

--- src/app/config/organizations.ts
+++ src/app/config/organizations.ts
@@ -1,14 +1,23 @@
 // Organization and assignment mappings
 export const ORGANIZATIONS = {
   "CS1632-Fall2025": {
-    name: "CS1632-Fall2025",
-    owner: "wonsunahn", // GitHub organization/owner name
+    name: "CS1632 (Fall2025)",
+    owner: "CS1632-Fall2025", // GitHub organization/owner name
     assignments: {
 
-      "Exercise 0: Java Assessment": "cs1632-fall2025-exercise-0-java-assessment-",
-      "Exercise 1: Test Plans": "cs1632-fall2025-exercise-1-test-plans-",
+      "Exercise 0: Java Assessment": "exercise-0-java-assessment",
+      "Exercise 1: Test Plans": "exercise-1-test-plans",
+      "Deliverable 1: Test Plans": "deliverable-1-test-plans",
     }
   },
+  "CS1622-Fall2025": {
+    name: "CS1622 (Fall2025)",
+    owner: "CS1622-Fall2025", // GitHub organization/owner name
+    assignments: {
+
+      "Project 1: Lexical Analyzer": "project-1-lexical-analyzer",
+    }
+  }
 } as const;
 
 export type OrganizationKey = keyof typeof ORGANIZATIONS;
