7405ccc8594daf7147cb402e93229faa70071950
wonsunahn <54322243+wonsunahn@users.noreply.github.com>
2026-02-25 04:09:35 +0900

    Add deliverable 3 and 4 for Playwright and Performance Testing

--- src/app/config/organizations.ts
+++ src/app/config/organizations.ts
@@ -10,7 +10,9 @@ export const ORGANIZATIONS = {
       "Exercise 2: Unit Testing": "exercise-2-unit-testing",
       "Deliverable 2: Unit Testing": "deliverable-2-unit-testing",
       "Exercise 3: Playwright Testing": "exercise-3-playwright-testing",
+      "Deliverable 3: Playwright Testing": "deliverable-3-playwright-testing",
       "Exercise 4: Performance Testing": "exercise-4-performance-testing",
+      "Deliverable 4: Performance Testing": "deliverable-4-performance-testing",
       "Supplementary Exercise 1: Cucumber Testing": "supplementary-exercise-1-cucumber-testing"
     }
   }
